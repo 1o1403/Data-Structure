5c465f76b12e5419059ce0749671ed46d9a1d18f
chaseyeon <chaseyeeeeeon@gmail.com>
2026-04-02 22:46:57 +0900

    이중연결리스트 테스트 reverse_traverse가능, 내 앞 노드에 대한 정보를 저장하고 있기에.

---
+++ DoublyLinkedList_main.py
@@ -0,0 +1,14 @@
+from DoublyLinkedList import DoublyLinkedList
+
+double = DoublyLinkedList()
+double.insert(0, 10)
+double.insert(1, 20)
+double.insert(2, 30)
+double.insert(3, 40)
+double.insert(4, 50)
+double.insert(5, 60)
+double.insert(6, 70)
+
+double.traverse()
+
+double.reverse_traverse()
